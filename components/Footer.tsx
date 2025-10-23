cd0167b34b9ad802607c874c58e459740ad0c0d0
aadicoder1 <aadityasahu1203@gmail.com>
2025-10-23 20:00:00 +0530

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,31 +1,13 @@
 import { GENERAL_INFO } from '@/lib/data';
-import { GitFork, Star } from 'lucide-react';
-
-interface RepoStats {
-    stargazers_count: number;
-    forks_count: number;
-}
 
 const Footer = async () => {
-    const repoStats = await fetch(
-        'https://api.github.com/repos/tajmirul/portfolio-2.0',
-        {
-            next: {
-                revalidate: 60 * 60, // 1 hour
-            },
-        },
-    );
-
-    const { stargazers_count, forks_count } =
-        (await repoStats.json()) as RepoStats;
-
     return (
         <footer className="text-center pb-5" id="contact">
             <div className="container">
                 <p className="text-lg">Get In Touch</p>
-                <p>I'm currently open to new opportunities and collaborations.</p>
+                <p>I&apos;m currently open to new opportunities and collaborations.</p>
                 <p>Whether you have a project in mind, a question, or just want to say hi, my inbox is always open.</p>
-                <p>I'll try my best to get back to you!</p>
+                <p>I&apos;ll try my best to get back to you!</p>
                 <a
                     href={`mailto:${GENERAL_INFO.email}`}
                     className="text-3xl sm:text-4xl font-anton inline-block mt-5 mb-10 hover:underline"
@@ -33,15 +15,8 @@ const Footer = async () => {
                     {GENERAL_INFO.email}
                 </a>
 
-                <div className="">
-                    <a
-                        href="https://github.com/Tajmirul/portfolio-2.0"
-                        target="_blank"
-                        className="leading-none text-muted-foreground hover:underline hover:text-white"
-                    >
-                        Built by Aaditya Sahu.
-                        
-                    </a>
+                <div>
+                        Built by Aaditya Sahu
                 </div>
             </div>
         </footer>
